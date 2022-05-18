dd550f8ed96cfa4b29a0b794a25177d77efb527f
Martinluther <martinlutherakuhwa@gmail.com>
2022-05-18 04:22:30 +0100

    first commit

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer } from "./Footer";
+export { default as Footerbanner } from "./Footerbanner";
+export { default as HeroBanner } from "./HeroBanner";
+export { default as Cart } from "./Cart";
+export { default as Layout } from "./Layout";
+export { default as NavBar } from "./NavBar";
+export { default as Product } from "./Product";
---
+++ pages/success.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useGlobalContext } from "../context/GlobalContext";
+import { runFireworks } from "../lib/UTILS.JS";
+
+const success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } =
+    useGlobalContext();
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantity(0);
+    runFireworks();
+  });
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order</h2>
+        <p className="email-msg"> check your email box for your receipt</p>
+        <p className="description">
+          ifyou have any questions, please email
+          <a className="email" href="mailto:order@example.com">
+            order@example.com
+          </a>
+        </p>
+        <Link href={"/"}>
+          <button type="button" className="btn" width="300px">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default success;
